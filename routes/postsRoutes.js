524a29715a2255040791b936e3b70ac078cc7f44
Ola Ishola <olaishola@hotmail.co.uk>
2022-08-12 16:00:50 +0100

    refactor createPost routes

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -4,8 +4,8 @@ const { jwtTokenVerification } = require('../controllers/auth/auth');
 const { createPost, getPosts, getPostsByUserId, getPostById } = require('../controllers/posts');
 const upload = require('../utils/multer');
 
-router.post('/', upload.single('image'), jwtTokenVerification, createPost);
 router.get('/', jwtTokenVerification, getPosts);
+router.post('/', upload.single('image'), jwtTokenVerification, createPost);
 router.get('/user', jwtTokenVerification, getPostsByUserId);
 router.get('/user/:id', jwtTokenVerification, getPostById);
 
